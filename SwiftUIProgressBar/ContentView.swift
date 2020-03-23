6886a7de323d7e85de0d76c6f93de18fdfa050ec
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-23 09:58:45 +0200

    init

--- SwiftUIProgressBar/ContentView.swift
+++ SwiftUIProgressBar/ContentView.swift
@@ -9,8 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress = 0
+    @State private var currentProgress = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(Color(.secondarySystemBackground))
+                    .frame(width: 300, height: 24)
+                    .cornerRadius(8)
+                
+                LinearGradient(gradient: Gradient(colors: [
+                    progress < 300 ? .orange : Color(.systemTeal),
+                    progress < 300 ? .pink : .blue
+                ]), startPoint: .top, endPoint: .bottom)
+                    .frame(width: CGFloat(progress), height: 24)
+                    .cornerRadius(8)
+            }.padding(.bottom)
+            
+            HStack {
+                Text(progress < 300 ? "\(currentProgress) of 6" : "COMPLETE")
+                    .bold()
+                Spacer()
+                Text("\(progress / 3)%")
+            }
+            if progress == 300 {
+                Button(action: {
+                    self.progress = 0
+                    self.currentProgress = 0
+                }) {
+                    Image(systemName: "arrow.counterclockwise")
+                }
+                .padding()
+            }
+        }
+        .frame(width: 272)
+        .onTapGesture {
+            if self.progress < 300 {
+                self.progress += 50
+                self.currentProgress += 1
+            }
+        }
     }
 }
 
